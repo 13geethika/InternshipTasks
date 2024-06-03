9a56c128e1429fefd7ff8ba399806df6f0c1eb00
13geethika <109813734+13geethika@users.noreply.github.com>
2024-06-04 00:07:06 +0530

    Update applications.js

--- applications.js
+++ applications.js
@@ -1,35 +1,38 @@
 const express = require('express');
 const Application = require('../models/Application');
-const User = require('../models/User');
 
 const router = express.Router();
 
-// Submit Application
 router.post('/emailreq', async (req, res) => {
-  const { firstName, lastName, domain, phoneNumber } = req.body;
+  const { firstName, lastName, domain, phoneNumber, personalEmail } = req.body;
 
   try {
-    // Create a new application with the provided data
-    const newApplication = new Application({ firstName, lastName, domain, phoneNumber });
-    await newApplication.save();
+    const application = new Application({ firstName, lastName, personalEmail, domain, phoneNumber, submit: true });
+    await application.save();
 
-    res.status(201).json({ msg: 'Application submitted successfully', application: newApplication });
+    res.status(201).json({ msg: 'Application submitted successfully', application: application }); 
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
   }
 });
 
-
-// Get All Applications (for Domain Heads)
+// All Applications (for Domain Heads)
 router.get('/emailreq', async (req, res) => {
-    try {
+  const { personalEmail } = req.query; 
+  try {
+    if (personalEmail) {
+      const applications = await Application.find({ personalEmail: personalEmail });
+      res.json(applications);
+    } else {
       const applications = await Application.find({});
       res.json(applications);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
     }
-  });
-  
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
 
 // Verify or Decline Application
 router.put('/:id', async (req, res) => {
