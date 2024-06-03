8a058d9aff71f960506b2f7c14a6c8e112789fc8
13geethika <109813734+13geethika@users.noreply.github.com>
2024-06-04 00:08:01 +0530

    Update Application.js

--- Application.js
+++ Application.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
 const ApplicationSchema = new mongoose.Schema({
+  
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  personalEmail: { type: String, required: true },
   domain: { type: String, required: true },
   phoneNumber: { type: String, required: true },
-  status: { type: String, default: 'applied' },
+  submit:{type:Boolean, default:false},
+  status: { type: String, default: 'applied' }
+  
 });
 
 module.exports = mongoose.model('Application', ApplicationSchema);
